47b1b45fea2cf4d9077b20b9d919313d637e94e8
Roman Wendelboe <rwendelboe@yahoo.com>
2015-07-16 16:58:03 -0600

    Changes to dialog and index.

--- Web.UI/Content/js/lib/msgBox.js
+++ Web.UI/Content/js/lib/msgBox.js
@@ -11,26 +11,7 @@ define(function(require) {
 
             var title = "Information",
                 message = "",
-                callback,
-                tittleBgColor = "#275F98",
-                tittleFontColor = "#ffffff",
-                messageBgColor = "#ffffff",
-                messageFontColor = "#000000";
-
-            if (options !== undefined) {
-                if (options.messageFontColor !== undefined && options.messageFontColor !== "") {
-                    messageFontColor = options.messageFontColor;
-                }
-                if (options.messageBgColor !== undefined && options.messageBgColor !== "") {
-                    messageBgColor = options.messageBgColor;
-                }
-                if (options.tittleBgColor !== undefined && options.tittleBgColor !== "") {
-                    tittleBgColor = options.tittleBgColor;
-                }
-                if (options.tittleFontColor !== undefined && options.tittleFontColor !== "") {
-                    tittleFontColor = options.tittleFontColor;
-                }
-            }
+                callback;
 
             if (messageCallback !== undefined) {
                 if (Object.prototype.toString.call(messageCallback) === "[object Function]") {
@@ -44,7 +25,7 @@ define(function(require) {
             if (!messageText) {
                 messageText = message;
             }
-            messageText = "<i class=\"fmf24-exclamation\" style=\" font-size: 22px; vertical-align: middle; margin-right: .25em; margin-top : -0.225em\"></i>" + messageText;
+            messageText = "<i class=\"fa fmf-exclamation24 msg-icons\"></i>" + messageText;
             try {
                 var $dialogContainer = $('#msgBox');
                 var $detachedChildren = $dialogContainer.children().detach();
@@ -76,16 +57,9 @@ define(function(require) {
                         }
                     }
                 });
-                var image = "cus-accept";
-//                $('#msgBox').parent().children(".ui-dialog-titlebar").prepend('<span style="float:left; margin-top: 7px; margin-right: .5em;" class="fa cus-About"></span>');
+
                 $('#msgBox').dialog("open");
                 $('#msgBox').dialog("option", "position", "center");
-
-
-//                $(".ui-widget-content").css("color", messageFontColor);
-//                $(".ui-widget-content").css("background", messageBgColor);
-//                $(".ui-widget-header").css("background", tittleBgColor);
-//                $(".ui-dialog-title").css("color", tittleFontColor);
 
                 msgBox.byPassjQueryUI();
 
@@ -99,26 +73,7 @@ define(function(require) {
 
             var title = "Warning",
                 message = "",
-                callback,
-                tittleBgColor = "#275F98",
-                tittleFontColor = "#ffffff",
-                messageBgColor = "#ffffff",
-                messageFontColor = "#000000";
-
-            if (options !== undefined) {
-                if (options.messageFontColor !== undefined && options.messageFontColor !== "") {
-                    messageFontColor = options.messageFontColor;
-                }
-                if (options.messageBgColor !== undefined && options.messageBgColor !== "") {
-                    messageBgColor = options.messageBgColor;
-                }
-                if (options.tittleBgColor !== undefined && options.tittleBgColor !== "") {
-                    tittleBgColor = options.tittleBgColor;
-                }
-                if (options.tittleFontColor !== undefined && options.tittleFontColor !== "") {
-                    tittleFontColor = options.tittleFontColor;
-                }
-            }
+                callback;
 
             if (messageCallback !== undefined) {
                 if (Object.prototype.toString.call(messageCallback) === "[object Function]") {
@@ -132,7 +87,7 @@ define(function(require) {
             if (!messageText) {
                 messageText = message;
             }
-            messageText = "<i class=\"fmf24-warning\" style=\" font-size: 22px; vertical-align: middle; margin-right: .25em; margin-top : -0.225em\"></i>" + messageText;
+            messageText = "<i class=\"fa fmf-warning24 msg-icons\"></i>" + messageText;
             try {
                 var $dialogContainer = $('#msgBox');
                 var $detachedChildren = $dialogContainer.children().detach();
@@ -164,16 +119,71 @@ define(function(require) {
                         }
                     }
                 });
-                var image = "cus-accept";
-                //                $('#msgBox').parent().children(".ui-dialog-titlebar").prepend('<span style="float:left; margin-top: 7px; margin-right: .5em;" class="fa cus-About"></span>');
+
                 $('#msgBox').dialog("open");
                 $('#msgBox').dialog("option", "position", "center");
 
+                msgBox.byPassjQueryUI();
 
-                //                $(".ui-widget-content").css("color", messageFontColor);
-                //                $(".ui-widget-content").css("background", messageBgColor);
-                //                $(".ui-widget-header").css("background", tittleBgColor);
-                //                $(".ui-dialog-title").css("color", tittleFontColor);
+            } catch (error) {
+                window.console.log(error);
+            }
+
+        },
+
+        error: function (messageText, messageTitle, messageCallback, options) {
+
+            var title = "Error",
+                message = "",
+                callback;
+
+            if (messageCallback !== undefined) {
+                if (Object.prototype.toString.call(messageCallback) === "[object Function]") {
+                    callback = messageCallback;
+                }
+            }
+
+            if (!messageTitle) {
+                messageTitle = title;
+            }
+            if (!messageText) {
+                messageText = message;
+            }
+            messageText = "<i class=\"fa fmf-warning24 msg-icons\"></i>" + messageText;
+            try {
+                var $dialogContainer = $('#msgBox');
+                var $detachedChildren = $dialogContainer.children().detach();
+                $('<div id="msgBox"></div>').html(messageText).dialog({
+                    title: messageTitle,
+                    width: 550,
+                    open: function () {
+                        $detachedChildren.appendTo($dialogContainer);
+                    },
+                    close: function (event, ui) {
+                        event.preventDefault();
+                        if (callback !== undefined) {
+                            callback();
+                        } else {
+                            $(this).dialog('destroy').remove();
+                        }
+                    },
+                    buttons: {
+                        "0": {
+                            id: 'ok',
+                            text: 'OK',
+                            icons: { primary: "okIcon" },
+                            click: function (event) {
+                                event.preventDefault();
+                                $(this).dialog("close");
+                            },
+
+                            "class": "btn-u btn-brd btn-brd-hover rounded btn-u-green"
+                        }
+                    }
+                });
+
+                $('#msgBox').dialog("open");
+                $('#msgBox').dialog("option", "position", "center");
 
                 msgBox.byPassjQueryUI();
 
@@ -187,28 +197,7 @@ define(function(require) {
 
             var title = "Question",
                 message = "",
-                tittleBgColor = "#275F98",
-                tittleFontColor = "#ffffff",
-                messageBgColor = "#ffffff",
-                messageFontColor = "#000000",
                 result;
-
-            if (options !== undefined) {
-                if (options.messageFontColor !== undefined && options.messageFontColor !== "") {
-                    messageFontColor = options.messageFontColor;
-                }
-                if (options.messageBgColor !== undefined && options.messageBgColor !== "") {
-                    messageBgColor = options.messageBgColor;
-                }
-
-                if (options.tittleBgColor !== undefined && options.tittleBgColor !== "") {
-                    tittleBgColor = options.tittleBgColor;
-                }
-
-                if (options.tittleFontColor !== undefined && options.tittleFontColor !== "") {
-                    tittleFontColor = options.tittleFontColor;
-                }
-            }
 
             if (!messageTitle) {
                 messageTitle = title;
@@ -216,7 +205,7 @@ define(function(require) {
             if (!messageText) {
                 messageText = message;
             }
-            messageText = "<i class=\"fmf24-question\" style=\" font-size: 22px; vertical-align: middle; margin-right: .25em; margin-top : -0.225em\"></i>" + messageText;
+            messageText = "<i class=\"fa fmf-question24 msg-icons\" ></i>" + messageText;
             try {
                 var $dialogContainer = $('#msgBox');
                 var $detachedChildren = $dialogContainer.children().detach();
@@ -276,33 +265,14 @@ define(function(require) {
                     }
                 });
 
-                var image = "cus-accept";
-//                $('#msgBox').parent().children(".ui-dialog-titlebar").prepend('<span style="float:left; margin-top: 2px; margin-right: .2em;" class="fa fmf24-question"></span>');
                 $('#msgBox').dialog("open");
                 $('#msgBox').dialog("option", "position", "center");
-
-//                $(".ui-widget-content").css("color", messageFontColor);
-//                $(".ui-widget-content").css("background", messageBgColor);
-//                $(".ui-widget-header").css("background", tittleBgColor);
-//                $(".ui-dialog-title").css("color", tittleFontColor);
 
                 msgBox.byPassjQueryUI();
 
             } catch (error) {
                 window.console.log(error);
             }
-        },
-
-        byPassjQueryUI: function() {
-            $(".ui-button-text-only .ui-button-text").css("padding-left", "20px");
-            $(".ui-button-text-only .ui-button-text").css("padding-right", "20px");
-            $(".ui-button-text-only .ui-button-text").css("padding-top", "5px");
-            $(".ui-button-text-only .ui-button-text").css("padding-bottom", "5px");
-            $(".ui-button-text-only .ui-button-text").css("font-size", "13px");
-
-            $(".ui-dialog").css("border-width", "1px");
-            $(".ui-dialog").css("border-style", "solid");
-            $(".ui-dialog").css("border-color", "#76A0F8");
         },
 
         ReplaceMessage: function() {
